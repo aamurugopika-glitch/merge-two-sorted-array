6498a6e1ea6bfbe0114d1525d7d9ada7c4374b97
aamurugopika-glitch <aamurugopika@gmail.com>
2025-12-29 15:42:41 +0530

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,23 @@
+def mergeTwoSortedArray(arr1,arr2):
+    n1=len(arr1)
+    n2=len(arr2)
+    l=r=0
+    out=[]
+    while l<n1 and r<n2:
+        if arr1[l]<=arr2[r]:
+            out.append(arr1[l])
+            l+=1 
+        else:
+            out.append(arr2[r])
+            r+=1
+    while l<n1:
+        out.append(arr1[l])
+        l+=1 
+    while r<n2:
+        out.append(arr2[r])
+        r+=1 
+    return out
+arr1=[1,2,5,7,11,13,25]
+arr2=[5,7,9,11,27,32,35,45]
+print(mergeTwoSortedArray(arr1,arr2))
+  
